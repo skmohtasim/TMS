db67ac49cf455831c45bb267c75b71dfef1cec1a
SK. Mohtasim Billah <polinaust87@gmail.com>
2017-07-29 21:17:40 +0600

    project initialized

---
+++ AssetTrackingApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetTrackingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string name)
+        {
+
+            return View();
+        }
+
+        public ActionResult JqueryWorkout()
+        {
+            
+            return View();
+        }
+
+        public JsonResult Ajax()
+        {
+            return Json(new { id = 1, value = "new" },JsonRequestBehavior.AllowGet);
+        }
+    }
+}
